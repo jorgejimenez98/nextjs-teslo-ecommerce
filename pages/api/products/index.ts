6e77b723370b37fc36273993ba458118799befa4
Jorge Jimenez <jjimenezdiaz@quental.com>
2022-12-26 19:31:55 +0100

    add filtering on api

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { ProductModel } from '../../../models'
 import { IProduct } from '../../../interfaces'
-import { db } from '../../../database'
+import { db, SHOP_CONSTANTS } from '../../../database'
 
 type Response = 
 | { message: string }
@@ -19,8 +19,19 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Respon
 }
 
 const getProducts = async (req: NextApiRequest, res: NextApiResponse<Response>) => {
+    const { gender = 'all' } = req.query;
+    let condition = {};
+
+    if ( gender !== 'all' && SHOP_CONSTANTS.validGenders.includes(`${gender}`) ) {
+        condition = { gender };
+    }
+
     await db.connect()
-    const products = await ProductModel.find({}).lean()
+    
+    const products = await ProductModel.find(condition)
+        .select('title images price inStock slug -_id')
+        .lean();
+
     await db.disconnect()
     return res.status(200).json(products)
 }
